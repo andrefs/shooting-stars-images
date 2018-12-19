cc4e6e8cfedbb265bf55f717051801ffccd75eb2
André Santos <andrefs@andrefs.com>
2018-12-19 01:04:29 +0000

    50 artists with images

---
+++ post-items.sh
@@ -0,0 +1,51 @@
+API_ENDPOINT=http://localhost:15111/items?token=
+curl -F  name="Rihanna"                          -F "image=@rihanna.jpg"        $API_ENDPOINT
+curl -F  name="Matt Bellamy (Muse)"              -F "image=@muse.jpg"           $API_ENDPOINT
+curl -F  name="Steven Tyler (Aerosmith)"         -F "image=@aerosmith.jpg"      $API_ENDPOINT
+curl -F  name="John Lennon (The Beatles)"        -F "image=@johnlennon.jpg"     $API_ENDPOINT
+curl -F  name="Prince"                           -F "image=@prince.jpg"         $API_ENDPOINT
+curl -F  name="Elton John"                       -F "image=@eltonjohn.jpg"      $API_ENDPOINT
+curl -F  name="Madonna"                          -F "image=@madonna.jpg"        $API_ENDPOINT
+curl -F  name="Adele"                            -F "image=@adele.jpg"          $API_ENDPOINT
+curl -F  name="Eddie Vedder (Pearl Jam)"         -F "image=@eddievedder.jpg"    $API_ENDPOINT
+curl -F  name="Justin Bieber"                    -F "image=@bieber.jpg"         $API_ENDPOINT
+curl -F  name="Jennifer Lopez"                   -F "image=@jeniferlopez.jpg"   $API_ENDPOINT
+curl -F  name="Bruce Springstein"                -F "image=@bruce.jpg"          $API_ENDPOINT
+curl -F  name="Tina Turner"                      -F "image=@tinaturner.jpg"     $API_ENDPOINT
+curl -F  name="Sting"                            -F "image=@sting.jpg"          $API_ENDPOINT
+curl -F  name="Chris Martin (Coldplay)"          -F "image=@coldplay.jpg"       $API_ENDPOINT
+curl -F  name="Edith Piaf"                       -F "image=@piaf.jpg"           $API_ENDPOINT
+curl -F  name="Eminem"                           -F "image=@eminem.jpg"         $API_ENDPOINT
+curl -F  name="Taylor Swift"                     -F "image=@swift.jpg"          $API_ENDPOINT
+curl -F  name="Cher"                             -F "image=@cher.jpg"           $API_ENDPOINT
+curl -F  name="Amy Whinehouse"                   -F "image=@amy.jpg"            $API_ENDPOINT
+curl -F  name="Mick Jagger (The Rolling Stones)" -F "image=@mickjagger.jpg"     $API_ENDPOINT
+curl -F  name="Freddie Mercury (Queen)"          -F "image=@queen.jpg"          $API_ENDPOINT
+curl -F  name="Jon Bon Jovi (Bon Jovi)"          -F "image=@bonjovi.jpg"        $API_ENDPOINT
+curl -F  name="Adam Levine (Maroon 5)"           -F "image=@maroon5.jpg"        $API_ENDPOINT
+curl -F  name="Michael Jackson"                  -F "image=@michaeljackson.jpg" $API_ENDPOINT
+curl -F  name="Kurt Cobain (Nirvana)"            -F "image=@nirvana.jpg"        $API_ENDPOINT
+curl -F  name="Beyonce"                          -F "image=@beyonce.jpg"        $API_ENDPOINT
+curl -F  name="Frank Sinatra"                    -F "image=@sinatra.jpg"        $API_ENDPOINT
+curl -F  name="Shakira"                          -F "image=@shakira.jpg"        $API_ENDPOINT
+curl -F  name="Justin Timberlake"                -F "image=@timberlake.jpg"     $API_ENDPOINT
+curl -F  name="Lenny Kravitz"                    -F "image=@lenny.jpg"          $API_ENDPOINT
+curl -F  name="Axl Rose (Guns N'Roses)"          -F "image=@axlrose.jpg"        $API_ENDPOINT
+curl -F  name="Bono (U2)"                        -F "image=@bono.jpg"           $API_ENDPOINT
+curl -F  name="Thom Yorke (Radiohead)"           -F "image=@radiohead.jpg"      $API_ENDPOINT
+curl -F  name="Luciano Pavarotti"                -F "image=@pavarotti.jpg"      $API_ENDPOINT
+curl -F  name="Bruno Mars"                       -F "image=@brunomars.jpg"      $API_ENDPOINT
+curl -F  name="Édith Piaf"                       -F "image=@edithpiaf.jpg"      $API_ENDPOINT
+curl -F  name="Robert Plant"                     -F "image=@ledzepplin.jpg"     $API_ENDPOINT
+curl -F  name="James Hetfield (Metallica)"       -F "image=@metallica.jpg"      $API_ENDPOINT
+curl -F  name="Bruce Dickinson"                  -F "image=@ironmaiden.jpg"     $API_ENDPOINT
+curl -F  name="Johnny Cash"                      -F "image=@cash.jpg"           $API_ENDPOINT
+curl -F  name="Lady Gaga"                        -F "image=@gaga.jpg"           $API_ENDPOINT
+curl -F  name="Brian Johnson (AC/DC)"            -F "image=@acdc.jpg"           $API_ENDPOINT
+curl -F  name="David Bowie"                      -F "image=@bowie.jpg"          $API_ENDPOINT
+curl -F  name="Whitney Houston"                  -F "image=@whitneyhouston.jpg" $API_ENDPOINT
+curl -F  name="Elvis Presley"                    -F "image=@elvis.jpg"          $API_ENDPOINT
+curl -F  name="Jim Morrison (The Doors)"         -F "image=@doors.jpg"          $API_ENDPOINT
+curl -F  name="Stacey Kent"                      -F "image=@staceykent.jpg"     $API_ENDPOINT
+curl -F  name="Bob Marley"                       -F "image=@bobmarley.jpg"      $API_ENDPOINT
+curl -F  name="Roger Waters (Pink Floyd)"        -F "image=@pinkfloyd.jpg"      $API_ENDPOINT
